be5c010cc7b870606c95895697c73afc88e51e10
Animesh-941 <77621064+Animesh-941@users.noreply.github.com>
2021-03-16 08:50:04 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,26 @@
+class script(object):
+
+    START_MSG = """ <b>Hi {}
+
+I'm a Image Editor Bot which Supports various modes
+
+For more click help....</b>"""
+
+    HELP_MSG = """Hey, Follow these Steps..
+
+<code>🌀 Send me any Image to Edit..</code>
+
+<code>🌀 Select the Corresponding mode that you need</code>
+
+<code>🌀 Your Edited Image will be Uploaded </code>
+
+© @Animesh_941"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b> Other Bots : @Librarian_Notes_Bot </b>
+
+⭕️<b> Developer : @AniMesH941 </b>
